2949a656761dee0dd07f0bdba75fe41950df329a
chouchong <wmk223@163.com>
2017-05-26 10:26:00 +0800

    wang.2.2

--- application/wap/controller_b2c/Pay.php
+++ application/wap/controller_b2c/Pay.php
@@ -208,19 +208,22 @@ class Pay extends Controller
             $out_trade_no = $_POST['out_trade_no'];
             // 支付宝交易号
             $trade_no = $_POST['trade_no'];
+            
             // 交易状态
             $trade_status = $_POST['trade_status'];
+            
             if ($_POST['trade_status'] == 'TRADE_FINISHED') {
                 // 判断该笔订单是否在商户网站中已经做过处理
                 // 如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
                 // 如果有做过处理，不执行商户的业务程序
                 // 注意：
                 // 退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
+                
                 // 调试用，写文本函数记录程序运行情况是否正常
                 // logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
                 $retval = $pay->onlinePay($out_trade_no, 2);
                 // $res = $order->orderOnLinePay($out_trade_no, 2);
-            } else{
+            } else 
                 if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
                     // 判断该笔订单是否在商户网站中已经做过处理
                     // 如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
